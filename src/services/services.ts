5d550b5ea1d49a89ab99581e5f3c68d64b80c827
janChorny <janchorny@tut.by>
2023-01-16 22:55:14 +0300

    feat: add general cluster module, update readme

--- src/services/services.ts
+++ src/services/services.ts
@@ -5,38 +5,37 @@ export class Services {
   private storage: User[] = [];
 
   public async getAllUsers(): Promise<User[]> {
-    return new Promise((res) => {
-      res(this.storage);
-    });
+      return new Promise((res) => {
+        res(this.storage);
+      });
   };
 
   public async createNewUser(user: User): Promise<User>{
-    return new Promise((res) => {
-      const addedUser = { id: v4(), ...user };
-      this.storage.push(addedUser);
-      res(addedUser);
-    });
+      return new Promise((res) => {
+        const addedUser = { id: v4(), ...user };
+        this.storage.push(addedUser);
+        res(addedUser);
+      });
   };
 
-  public async getUser(id: string): Promise<User | undefined> {
-    return new Promise((res) => {
-      const userToFind = this.storage.filter(el => el.id === id)[0];
-      res(userToFind);
-    });
+  public async getUser(id: string): Promise<User> {
+      return new Promise((res) => {
+        const userToFind = this.storage.filter(el => el.id === id)[0];
+        res(userToFind);
+      });
   };
 
   public async deleteUser(id: string): Promise<void> {
-    return new Promise((res) => {
-      this.storage = this.storage.filter(el => el.id !== id);
-      res();
-    });
+      return new Promise((res) => {
+        this.storage = this.storage.filter(el => el.id !== id);
+        res();
+      });
   };
 
   public async updateUser(id: string, userNewInfo: User): Promise<User>  {
-    return new Promise((res) => {
-      const userIndex = this.storage.findIndex(el => el.id === id);
-      this.storage[userIndex] = { id, ...userNewInfo };
-      res(this.storage[userIndex]);
-    });
+      return new Promise(async (res) => {
+        const user = Object.assign(await this.getUser(id), userNewInfo);
+        res(user)
+      });
   };
 }
